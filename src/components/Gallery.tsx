a73b31d508d68dc00f6aa4b8a33f82a3957fd2d2
vu.nguyen <vu.nguyen@axon-networks.com>
2024-10-24 12:55:23 +0700

    fix url image

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -3,63 +3,63 @@ import { Divider } from "antd";
 import { styled } from "@stitches/react";
 import "react-image-gallery/styles/css/image-gallery.css";
 
-const Wrapper = styled("div", {
-  background: "#efebe9",
-  backgroundImage: "url(./assets/GroovePaper.png)",
-  width: "100%",
+const Wrapper = styled('div', {
+    background: '#efebe9',
+    backgroundImage: 'url(./wedding-invitation/assets/GroovePaper.png)',
+    width: '100%',
 });
 
-const Title = styled("p", {
-  fontSize: "2.5vh",
-  fontWeight: "bold",
-  opacity: 0.85,
-  marginBottom: 0,
+const Title = styled('p', {
+    fontSize: '2.5vh',
+    fontWeight: 'bold',
+    opacity: 0.85,
+    marginBottom: 0,
 });
 
 const images = [
     {
-        original: './assets/anh_cuoi_1.jpg',
-        thumbnail: './assets/anh_cuoi_1.jpg',
+        original: './wedding-invitation/assets/anh_cuoi_1.jpg',
+        thumbnail: './wedding-invitation/assets/anh_cuoi_1.jpg',
     },
     {
-        original: './assets/anh_cuoi_2.jpg',
-        thumbnail: './assets/anh_cuoi_2.jpg',
+        original: './wedding-invitation/assets/anh_cuoi_2.jpg',
+        thumbnail: './wedding-invitation/assets/anh_cuoi_2.jpg',
     },
     {
-        original: './assets/anh_cuoi_3.jpg',
-        thumbnail: './assets/anh_cuoi_3.jpg',
+        original: './wedding-invitation/assets/anh_cuoi_3.jpg',
+        thumbnail: './wedding-invitation/assets/anh_cuoi_3.jpg',
     },
     {
-        original: './assets/anh_cuoi_4.jpg',
-        thumbnail: './assets/anh_cuoi_4.jpg',
+        original: './wedding-invitation/assets/anh_cuoi_4.jpg',
+        thumbnail: './wedding-invitation/assets/anh_cuoi_4.jpg',
     },
     {
-        original: './assets/anh_cuoi_5.jpg',
-        thumbnail: './assets/anh_cuoi_5.jpg',
+        original: './wedding-invitation/assets/anh_cuoi_5.jpg',
+        thumbnail: './wedding-invitation/assets/anh_cuoi_5.jpg',
     },
     {
-        original: './assets/anh_cuoi_6.jpg',
-        thumbnail: './assets/anh_cuoi_6.jpg',
+        original: './wedding-invitation/assets/anh_cuoi_6.jpg',
+        thumbnail: './wedding-invitation/assets/anh_cuoi_6.jpg',
     },
     {
-        original: './assets/anh_cuoi_7.jpg',
-        thumbnail: './assets/anh_cuoi_7.jpg',
+        original: './wedding-invitation/assets/anh_cuoi_7.jpg',
+        thumbnail: './wedding-invitation/assets/anh_cuoi_7.jpg',
     },
     {
-        original: './assets/anh_cuoi_8.jpg',
-        thumbnail: './assets/anh_cuoi_8.jpg',
+        original: './wedding-invitation/assets/anh_cuoi_8.jpg',
+        thumbnail: './wedding-invitation/assets/anh_cuoi_8.jpg',
     },
     {
-        original: './assets/anh_cuoi_9.jpg',
-        thumbnail: './assets/anh_cuoi_9.jpg',
+        original: './wedding-invitation/assets/anh_cuoi_9.jpg',
+        thumbnail: './wedding-invitation/assets/anh_cuoi_9.jpg',
     },
     {
-        original: './assets/anh_cuoi_10.jpg',
-        thumbnail: './assets/anh_cuoi_10.jpg',
+        original: './wedding-invitation/assets/anh_cuoi_10.jpg',
+        thumbnail: './wedding-invitation/assets/anh_cuoi_10.jpg',
     },
     {
-        original: './assets/anh_cuoi_11.jpg',
-        thumbnail: './assets/anh_cuoi_11.jpg',
+        original: './wedding-invitation/assets/anh_cuoi_11.jpg',
+        thumbnail: './wedding-invitation/assets/anh_cuoi_11.jpg',
     },
 ];
 
