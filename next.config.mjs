c3e948ac10f299c8fc45010d137425d39c5363a3
vu.nguyen <vu.nguyen@axon-networks.com>
2024-10-24 13:22:16 +0700

    fix next config add base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: '/wedding-invitation',
+  assetPrefix: '/wedding-invitation/',
   reactStrictMode: true,
 };
 
