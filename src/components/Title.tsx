ac6e6ddca4c5a69a651ae7b5e6ca7576746b3747
vu.nguyen <vu.nguyen@axon-networks.com>
2024-10-24 13:28:12 +0700

    fix absolute url 2

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -84,7 +84,7 @@ export default function Title({ data }: TitleProps) {
     const [isLoading, setIsLoading] = useState(true);
     const videoRef = useRef(null);
 
-    const images = ['/assets/anh_cuoi_4.jpg'];
+    const images = ['/wedding-invitation/assets/anh_cuoi_4.jpg'];
 
     useEffect(() => {
         // Function để tải hình ảnh
@@ -124,10 +124,13 @@ export default function Title({ data }: TitleProps) {
                     loop
                     muted
                     playsInline={true}
-                    poster="/assets/anh_cuoi_4.jpg"
+                    poster="/wedding-invitation/assets/anh_cuoi_4.jpg"
                     onPlay={handleVideoPlay}
                 >
-                    <source src="/assets/video2.mp4" type="video/mp4" />
+                    <source
+                        src="/wedding-invitation/assets/video2.mp4"
+                        type="video/mp4"
+                    />
                 </VideoBackground>
             )}
             <TitleWrapper>
