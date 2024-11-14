7b7e5910d5643c585ac2b06db55c4596a6164466
vu.nguyen <vu.nguyen@axon-networks.com>
2024-11-14 10:39:01 +0700

    update bridge address

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -55,13 +55,21 @@ export default function Location() {
                     <Title>Cô dâu</Title>
                     <div style={{ borderRadius: '30px' }}>
                         <iframe
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d621.6312110452416!2d105.73697937419799!3d20.969466812189758!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313452ffa3802155%3A0x9319cf85bcd88ebc!2zxJDDtG5nIExhLCBIb8OgaSDEkOG7qWMsIEjDoCBO4buZaQ!5e0!3m2!1svi!2s!4v1728962544590!5m2!1svi!2s"
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d931.38922136399!2d105.73706639506054!3d20.970302819903225!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3134530047060bbb%3A0xf5c642d1fcb397c2!2zeMaw4bufbmcgc-G6o24geHXhuqV0IG7hu5lpIHRo4bqldCBUUUMgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1731555482910!5m2!1svi!2s"
                             style={{ border: '0', borderRadius: '20px' }}
                             height="300"
                             allowFullScreen
                             loading="lazy"
                             referrerPolicy="no-referrer-when-downgrade"
                         ></iframe>
+                        {/* <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d621.6312110452416!2d105.73697937419799!3d20.969466812189758!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313452ffa3802155%3A0x9319cf85bcd88ebc!2zxJDDtG5nIExhLCBIb8OgaSDEkOG7qWMsIEjDoCBO4buZaQ!5e0!3m2!1svi!2s!4v1728962544590!5m2!1svi!2s"
+                            style={{ border: '0', borderRadius: '20px' }}
+                            height="300"
+                            allowFullScreen
+                            loading="lazy"
+                            referrerPolicy="no-referrer-when-downgrade"
+                        ></iframe> */}
                     </div>
                 </div>
             </LocationMap>
